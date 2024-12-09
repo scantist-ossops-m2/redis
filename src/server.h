cf71c00bcb413d7b2d2a9110cc9398f160627a3e
turly221 <feifan@scntist.com>
2024-12-09 04:54:32 +0000

    commit patch 23911773

--- src/server.h
+++ src/server.h
@@ -1728,6 +1728,7 @@ void protectClient(client *c);
 void unprotectClient(client *c);
 void initThreadedIO(void);
 client *lookupClientByID(uint64_t id);
+int authRequired(client *c);
 
 #ifdef __GNUC__
 void addReplyErrorFormat(client *c, const char *fmt, ...)
